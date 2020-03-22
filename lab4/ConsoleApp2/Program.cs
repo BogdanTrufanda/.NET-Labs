d55aca50aed00da0366f614ae09bfb9c71886bab
Bogdan <gitea@fake.local>
2020-03-22 10:45:03 +0200

    new3

---
+++ lab4/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ModelCodeFirst.Model.Model1;
+
+namespace ModelCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var database = new AlbumsContext())
+            {
+                var albums = new Album { AlbumName = "Album nou" };
+                database.Albums.Add(albums);
+                database.SaveChanges();
+
+                var qry = from x in database.Albums
+                            select x;
+                Console.WriteLine("Database albums");
+                foreach (var x in qry)
+                {
+                    Console.WriteLine(x.AlbumName);
+                }
+                Console.WriteLine("Press to exit..");
+                Console.ReadKey();
+            }
+        }
+    }
+
+
+}
