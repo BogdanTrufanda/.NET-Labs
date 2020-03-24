bc9b95967b7f0482d15d3e2d97fd5e3e3d1c2dba
Bogdan <gitea@fake.local>
2020-03-24 15:20:00 +0200

    new

--- lab6/WindowsFormsApp1/proxy.cs
+++ lab6/WindowsFormsApp1/proxy.cs
@@ -14,11 +14,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks; 
 using System.Windows.Forms;
+using System.Runtime.Serialization;
+
 namespace PostComment
 {
-    using System.Runtime.Serialization;
-    
-    
+    [DataContract(IsReference = true)]
+    public partial class CommentDTO
+    {
+        [DataMember]
+        public int CommentId { get; set; }
+        [DataMember]
+        public string CommentText { get; set; }
+        [DataMember]
+        public int PostId { get; set; }
+        [DataMember]
+        public int PostPostId { get; set; }
+        [DataMember]
+        public virtual PostDTO Post { get; set; }
+    }
+
+    [DataContract]
+    public partial class PostDTO
+    {
+        public PostDTO()
+        {
+            this.Comments = new List<CommentDTO>(); //new HashSet<Comment>();
+        }
+        [DataMember]
+        public int PostId { get; set; }
+        [DataMember]
+        public string Title { get; set; }
+        [DataMember]
+        public virtual List<CommentDTO> Comments { get; set; }
+    }
+
+ 
+
+
+
+
+
+
+
     [System.Diagnostics.DebuggerStepThroughAttribute()]
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Runtime.Serialization", "4.0.0.0")]
     [System.Runtime.Serialization.DataContractAttribute(Name="Post", Namespace="http://schemas.datacontract.org/2004/07/PostComment", IsReference=true)]
